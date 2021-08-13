c55fdc7e2a87c1dac26f512beb2c7e33aac8b5ce
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-08-13 21:33:55 +0430

    add some styles

--- src/App.js
+++ src/App.js
@@ -17,14 +17,16 @@ function App() {
   return (
     <div>
       <h1>this is a redux saga</h1>
-      {users &&
-        users.map((user, index) => (
-          <div key={index}>
-            <h2>name: {user.name}</h2>
-            <h3>username: {user.username}</h3>
-            <p>emil: {user.email}</p>
-          </div>
-        ))}
+      <div className="cardBx">
+        {users &&
+          users.map((user, index) => (
+            <div className="user-card" key={index}>
+              <h2>name: {user.name}</h2>
+              <h3>username: {user.username}</h3>
+              <p>emil: {user.email}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
